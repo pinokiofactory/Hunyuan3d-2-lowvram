f206ddb6727ffbbacf8808b92ef1cda857bdf22f
cocktailpeanut <cocktailpeanuts@proton.me>
2025-01-24 23:50:49 -0500

    upgrade schema version

--- pinokio.js
+++ pinokio.js
@@ -1,6 +1,6 @@
 const path = require('path')
 module.exports = {
-  version: "3.2",
+  version: "3.3.20",
   title: "Hunyuan3D-2",
   description: "[NVIDIA ONLY] High-Resolution 3D Assets Generation with Large Scale Hunyuan3D Diffusion Models. https://github.com/Tencent/Hunyuan3D-2",
   icon: "icon.png",
