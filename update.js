64dcb8644467a0be38fa45053452d1702db4ac9b
cocktailpeanut <cocktailpeanuts@proton.me>
2025-03-19 11:06:02 -0700

    update

--- update.js
+++ update.js
@@ -11,26 +11,14 @@ module.exports = {
       message: "git pull"
     }
   }, {
-    method: "script.start",
+    method: "fs.rm",
     params: {
-      uri: "torch.js",
-      params: {
-        venv: "env",                // Edit this to customize the venv folder path
-        path: "app",                // Edit this to customize the path to start the shell from
-        // xformers: true   // uncomment this line if your project requires xformers
-      }
+      path: "app/env"    
     }
   }, {
-    method: "shell.run",
+    method: "script.start",
     params: {
-      build: true,
-      venv: "env",                // Edit this to customize the venv folder path
-      path: "app",                // Edit this to customize the path to start the shell from
-      message: [
-        "uv pip install --no-build-isolation diso==0.1.4",
-        "uv pip install -r requirements.txt",
-        "uv pip install sentencepiece"
-      ]
+      uri: "install.js"
     }
   }]
 }
