3d0dda3b6bfad27ca30ca6f2688b35060fda0a04
cocktailpeanut <cocktailpeanuts@proton.me>
2025-03-19 10:26:46 -0700

    update

--- install.js
+++ install.js
@@ -41,6 +41,7 @@ module.exports = {
       params: {
         venv: "env",                // Edit this to customize the venv folder path
         path: "app",                // Edit this to customize the path to start the shell from
+        build: true,
         message: [
           "uv pip install -r requirements.txt",
           "uv pip install sentencepiece"
