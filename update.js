1276792d32251589c47554d28f34f38df2de526b
cocktailpeanut <cocktailpeanuts@proton.me>
2025-03-19 10:31:29 -0700

    update

--- update.js
+++ update.js
@@ -11,9 +11,14 @@ module.exports = {
       message: "git pull"
     }
   }, {
-    method: "script.start",
+    method: "shell.run",
     params: {
-      uri: "install.js"
+      venv: "env",                // Edit this to customize the venv folder path
+      path: "app",                // Edit this to customize the path to start the shell from
+      message: [
+        "uv pip install -r requirements.txt",
+        "uv pip install sentencepiece"
+      ]
     }
   }]
 }
