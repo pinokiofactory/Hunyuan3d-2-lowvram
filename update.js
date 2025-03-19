9022c816e16d637f756f6d1d731edd2854d3b759
cocktailpeanut <cocktailpeanuts@proton.me>
2025-03-19 11:09:39 -0700

    update

--- update.js
+++ update.js
@@ -15,10 +15,10 @@ module.exports = {
     params: {
       path: "app/env"    
     }
-  }, {
-    method: "script.start",
-    params: {
-      uri: "install.js"
-    }
+//  }, {
+//    method: "script.start",
+//    params: {
+//      uri: "install.js"
+//    }
   }]
 }
